01f1afd59eaa9beb3e812eadc20ee58331b89437
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-03-22 23:08:13 +0530

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './index.css'
 
-function App() {
+const books = [
+  {
+    Image: "https://bookbins.in/wp-content/uploads/2022/12/Everything-Is-Fucked-A-Book-About-Hope-Mark-Manson-Buy-Online-Bookbins-1.png",
+    Title: "Everything is Fucked",
+    Author: "Mark Manson"
+  },
+  {
+    Image: "https://bookbins.in/wp-content/uploads/2022/01/Ugly-Love-Colleen-Hoover-Buy-Online-Bookbins-1-600x643.png",
+    Title: "Ugly Love",
+    Author: "Colleen Hoover"
+  },
+  {
+    Image: "https://bookbins.in/wp-content/uploads/2022/12/Twisted-Lies-Ana-Huang-Buy-Online-Bookbins-1.png",
+    Title: "Twisted Lies",
+    Author: "Ana Huang"
+  },
+  {
+    Image: "https://bookbins.in/wp-content/uploads/2022/01/The-Midnight-Libary-Matt-Haig-Buy-Online-Bookbins-1.png",
+    Title: "The Midnight Library",
+    Author: "Matt Haig"
+  }
+]
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className='booklist'>
+      {books.map((book)=>{
+        const {Image, Title, Author} = book;
+        return <Book Image={Image} Title={Title} Author={Author} />;
+      })}
+    </section>
+  )
 }
 
-export default App;
+const Book = (props) => {
+const {Image, Title, Author} = props;
+  return (
+    <article className='book'>
+      <img src={Image} alt={Title} />
+      <h2>{Title }</h2>
+      <h4>{Author}</h4>
+    </article>
+  )
+}
+
+
+export default App
