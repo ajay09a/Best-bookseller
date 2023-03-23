3dbfa53d10a9cc9186f47c5653d8a4a7ba400b54
ajay09a <92861422+ajay09a@users.noreply.github.com>
2023-03-23 12:01:07 +0530

    creating click function and indexing

--- src/App.js
+++ src/App.js
@@ -5,44 +5,54 @@ const books = [
   {
     Image: "https://bookbins.in/wp-content/uploads/2022/12/Everything-Is-Fucked-A-Book-About-Hope-Mark-Manson-Buy-Online-Bookbins-1.png",
     Title: "Everything is Fucked",
-    Author: "Mark Manson"
+    Author: "Mark Manson",
+    id: 1
   },
   {
     Image: "https://bookbins.in/wp-content/uploads/2022/01/Ugly-Love-Colleen-Hoover-Buy-Online-Bookbins-1-600x643.png",
     Title: "Ugly Love",
-    Author: "Colleen Hoover"
+    Author: "Colleen Hoover",
+    id: 2
   },
   {
     Image: "https://bookbins.in/wp-content/uploads/2022/12/Twisted-Lies-Ana-Huang-Buy-Online-Bookbins-1.png",
     Title: "Twisted Lies",
-    Author: "Ana Huang"
+    Author: "Ana Huang",
+    id: 3
   },
   {
     Image: "https://bookbins.in/wp-content/uploads/2022/01/The-Midnight-Libary-Matt-Haig-Buy-Online-Bookbins-1.png",
     Title: "The Midnight Library",
-    Author: "Matt Haig"
+    Author: "Matt Haig",
+    id: 4
   }
 ]
 
 const App = () => {
+  const getbook = (id)=>{
+    const book = books.find((book)=> book.id === id)
+    console.log(book);
+  }
   return (
     <section className='booklist'>
-      {books.map((book)=>{
+      {books.map((book, index)=>{
         // const {Image, Title, Author} = book;
         // return <Book Image={Image} Title={Title} Author={Author} />;
-        return <Book {...book} />
+        return <Book {...book} getbook={getbook} number={index} />
       })}
     </section>
   )
 }
 
 const Book = (props) => {
-const {Image, Title, Author} = props;
+const {Image, Title, Author, id, getbook, number} = props;
   return (
     <article className='book'>
       <img src={Image} alt={Title} />
       <h2>{Title }</h2>
       <h4>{Author}</h4>
+      <span className='number'>{number+1}</span>
+      <button className='clickbtn' onClick={()=>getbook(id)}>Click</button>
     </article>
   )
 }
